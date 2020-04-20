5e183da3f829ec5a7da677bb3416899f0e2f7f14
Ella <ella.wg@hotmail.com>
2020-04-20 17:22:46 +0200

    Add work card

--- src/pages/work/[id]/index.tsx
+++ src/pages/work/[id]/index.tsx
@@ -2,40 +2,14 @@ import Layout from "../../../components/organisms/Layout";
 import SideBar from "../../../components/molecules/SideBar";
 
 import { work } from "../../../store/work.js";
-import TextTitle from "../../../components/atoms/TextTitle";
-import Tags from "../../../components/molecules/Tags";
-import Text from "../../../components/atoms/Text";
-
-const sincerewines = require("../../../assets/work/sincerewines.png");
-const minimize = require("../../../assets/work/minimize.png");
+import WorkCard from "../../../components/organisms/WorkCard";
 
 const Project = ({ item }) => {
-  const getImageSrc = () => {
-    if (item.title === "minimize") {
-      return minimize;
-    } else if (item.title === "sincere wines") {
-      return sincerewines;
-    } else {
-      return undefined;
-    }
-  };
   return (
     <Layout>
       <SideBar title="BACK" link="/" />
       <div className="flex flex-row justify-end items-center w-full h-full">
-        <div className="bg-mint w-full lg:w-10/12 xl:w-4/5 h-full md:h-auto lg:px-16 p-10 lg:py-20 flex flex-row flex-wrap md:flex-no-wrap">
-          <div className="flex flex-col">
-            <TextTitle className="text-lg">0{item.id}</TextTitle>
-            <TextTitle className="text-4xl mt-10">{item.title}</TextTitle>
-            <Tags tags={item.tags} />
-            <Text className="w-full md:w-3/5 m-0 md:ml-8 pt-12 text-dark-400 md:mb-0 sm:mb-8 mb-0">
-              {item.description}
-            </Text>
-          </div>
-          <div className="flex items-center flex-grow">
-            <img className="w-full" src={getImageSrc()}></img>
-          </div>
-        </div>
+        <WorkCard item={item} />
       </div>
     </Layout>
   );
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,5 +2,6 @@ export interface WorkData {
   title: string;
   id: number;
   path: string;
+  description: string;
   tags: string[];
 }
