0ec10035b025b6495bf649587ae8a0335de9e011
Ella <ella.wg@hotmail.com>
2020-04-24 11:17:57 +0200

    Fixes

--- src/components/molecules/SideBar.tsx
+++ src/components/molecules/SideBar.tsx
@@ -6,20 +6,11 @@ import Link from "next/link";
 interface Props {
   className?: string;
   title: string;
-  link?: string;
 }
 
-const SideBar: FunctionComponent<Props> = ({ title, link }) => (
+const SideBar: FunctionComponent<Props> = ({ title }) => (
   <div className="bg-dark whitespace-no-wrap text-white xl:h-full flex flex-col justify-start uppercase p-8">
-    {link ? (
-      <Link href={link}>
-        <span className="transform rotate-0 xl:rotate-90 w-4 cursor-pointer">
-          {title}
-        </span>
-      </Link>
-    ) : (
-      <span className="transform xl:rotate-90 w-4 rotate-0">{title}</span>
-    )}
+    <span className="transform xl:rotate-90 w-4 rotate-0">{title}</span>
   </div>
 );
 
